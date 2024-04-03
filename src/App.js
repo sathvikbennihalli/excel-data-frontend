d6d11bc5cac7b58307406354afba7a98f52a0b9d
Sathvik Bennihalli <sathvikbennihalli@Sathviks-MacBook-Air.local>
2024-04-03 13:32:00 +0530

    committing frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/agents")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "50px" }}>
+      <table style={{ borderCollapse: "collapse", width: "100%" }}>
+        <thead>
+          <tr>
+            <th style={tableHeaderStyle}>Agent ID</th>
+            <th style={tableHeaderStyle}>First Name</th>
+            <th style={tableHeaderStyle}>Last Name</th>
+            <th style={tableHeaderStyle}>Age</th>
+            <th style={tableHeaderStyle}>Email</th>
+            <th style={tableHeaderStyle}>Phone Number</th>
+            <th style={tableHeaderStyle}>Vehicle Type</th>
+            <th style={tableHeaderStyle}>License Plate</th>
+            <th style={tableHeaderStyle}>Delivery Area</th>
+            <th style={tableHeaderStyle}>Availability</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((d, i) => (
+            <tr key={i} style={(i % 2 === 0) ? tableRowEvenStyle : tableRowOddStyle}>
+              <td style={tableCellStyle}>{d.agent_id}</td>
+              <td style={tableCellStyle}>{d.first_name}</td>
+              <td style={tableCellStyle}>{d.last_name}</td>
+              <td style={tableCellStyle}>{d.age}</td>
+              <td style={tableCellStyle}>{d.email}</td>
+              <td style={tableCellStyle}>{d.phone_number}</td>
+              <td style={tableCellStyle}>{d.vehicle_type}</td>
+              <td style={tableCellStyle}>{d.license_plate}</td>
+              <td style={tableCellStyle}>{d.delivery_area}</td>
+              <td style={tableCellStyle}>{d.availability}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
+
+const tableHeaderStyle = {
+  backgroundColor: "#f2f2f2",
+  border: "1px solid #ddd",
+  padding: "12px",
+  textAlign: "left",
+};
+
+const tableCellStyle = {
+  border: "1px solid #ddd",
+  padding: "12px",
+  textAlign: "left",
+};
+
+const tableRowEvenStyle = {
+  backgroundColor: "#f9f9f9",
+};
+
+const tableRowOddStyle = {
+  backgroundColor: "#ffffff",
+};
 
 export default App;
